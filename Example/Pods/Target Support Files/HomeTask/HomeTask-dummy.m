ce213b6c5008f5097429682025ee349297e0e40c
luoxin <luoxin3116@gmail.com>
2017-12-26 23:04:39 +0800

    init spec

---
+++ Example/Pods/Target Support Files/HomeTask/HomeTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HomeTask : NSObject
+@end
+@implementation PodsDummy_HomeTask
+@end
---
+++ Example/Pods/Target Support Files/Pods-HomeTask_Tests/Pods-HomeTask_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HomeTask_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HomeTask_Tests
+@end
